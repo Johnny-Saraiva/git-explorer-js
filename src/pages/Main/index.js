eebabd8376752e72bac145bc47035a5f6ccc3ba0
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-07-01 18:38:32 -0300

    configured github api and loading animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,26 +1,66 @@
-import React from 'react';
-
-import { FaPlus, FaGithub } from 'react-icons/fa';
-
+import React, { Component } from 'react';
+import { FaPlus, FaGithub, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithub />
-        Repositories
-      </h1>
+import api from '../../services/api';
 
-      <Form>
-        <input type="text" placeholder="Add repositories" />
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
 
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleInputChange = (event) => {
+    this.setState({ newRepo: event.target.value });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithub />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add repositories"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
-
-export default Main;
